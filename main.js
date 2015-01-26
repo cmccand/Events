237f7740e0f440febd100075ecc12574c04f77df
Chris McCandlish <cmccand@gmail.com>
2015-01-25 23:10:17 -0500

    updated Events, more updates to come

--- main.js
+++ main.js
@@ -4,18 +4,29 @@ $(".TV").eq(0).click(function (event) {
   event.preventDefault();
   $(this).css("background-color", "blue");
 
-
+$(".TV").eq(0).click(function (event) {
+    event.preventDefault();
+    $(this).css("background-color", "red");
+});
 });
 
-$(".TV,.TV:before").eq(1).hover(function (event) {
+$(".TV").eq(1).hover(function (event) {
   event.preventDefault();
   $(this).css("border-radius", "50%");
+},  function() {
+  $(this).css("border-radius", "0%");
+
 });
 
 $(".TV").eq(2).mouseover(function (event) {
   event.preventDefault();
   $(this).css("opacity", ".5");
 });
+$(".TV").eq(2).mouseout(function (event) {
+  event.preventDefault();
+  $(this).css("opacity", "1");
+});
+
 
 $(".TV").eq(3).on("dblclick", function () {
   document.write("hi ya'll");
